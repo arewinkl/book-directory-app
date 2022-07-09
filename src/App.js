848722736c0f2b0b65400a3a78be7265182f9655
Alec Rewinkel <alecrewinkel@gmail.com>
2022-07-09 12:34:26 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  let [count, setCount] = useState(0);
+  const Add = () => {
+    setCount(count + 1);
+  };
+  const Minus = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Count: {count}</div>
+        <button onClick={Add}>Increment</button>
+        <button onClick={Minus}>Decrement</button>
       </header>
     </div>
   );
