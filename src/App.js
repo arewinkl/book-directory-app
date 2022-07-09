58fa9231f52fb230d2a57ba44e4fd281d9b6205a
Alec Rewinkel <alecrewinkel@gmail.com>
2022-07-09 12:59:26 -0600

    new pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import "./App.css";
 import { useState } from "react";
+import Header from "./Header";
 function App() {
-  let [count, setCount] = useState(0);
-  const Add = () => {
-    setCount(count + 1);
-  };
-  const Minus = () => {
-    setCount(count - 1);
-  };
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <div>Count: {count}</div>
-        <button onClick={Add}>Increment</button>
-        <button onClick={Minus}>Decrement</button>
       </header>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// import { useState } from "react";
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This Is the header</p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
