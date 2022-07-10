d911ce1642820f83738ee0e734de56249faf3585
Alec Rewinkel <alecrewinkel@gmail.com>
2022-07-10 10:52:58 -0600

    new page

---
+++ src/book-collection.js
@@ -0,0 +1,17 @@
+import "./App.css";
+// import { useState } from "react";
+function BookCollection() {
+  return (
+    <div className="Header">
+      <input></input>
+    </div>
+  );
+}
+
+export default BookCollection;
+
+//This page should allow the user to search for books and when they find a book that they like they should be able to add it to their collection.
+//Create different collections
+//Beable to add books to existing collections
+//be able to delete books from their collection
+//possible be able to sort their book collection.
